e42ab9cd2361df1c9787b242fa6327f966fe1ff4
Ricky Patel <rickyp@snowcoders.com>
2018-01-02 20:00:19 -0800

    Initial commit

--- src/component.test.tsx
+++ src/component.test.tsx
@@ -1,48 +1,45 @@
 import * as React from 'react';
 
-import { UnstyledButton, IUnstyledButtonProps } from "./component";
+import { Dialog, IDialogProps } from "./component";
 
 import { expect } from 'chai';
 import { shallow, configure } from "enzyme";
 
 // Configure enzyme
-import * as Adapter from 'enzyme-adapter-react-16';
+import * as Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-describe("UnstyledButton", () => {
+describe("Dialog", () => {
     it("Render", () => {
-        let wrapper = shallow(<UnstyledButton />);
+        let wrapper = shallow(<Dialog isVisible={false} />);
         expect(wrapper).to.have.length(1);
     });
 
     it("Default has classname", () => {
-        let wrapper = shallow(<UnstyledButton />);
-        let htmlButton = wrapper.find('button');
-        expect(htmlButton).to.have.length(1);
-        expect(htmlButton.props().className).to.contain('sci-react-unstyled-button');
+        let wrapper = shallow(<Dialog isVisible={false} />);
+        let rootElement = wrapper.first();
+        expect(rootElement.props().className).to.contain('sci-react-dialog');
     });
 
     it("Disable style has no classname", () => {
-        let wrapper = shallow(<UnstyledButton className="blue" isBaseStylesDisabled={true} />);
-        let htmlButton = wrapper.find('button');
-        expect(htmlButton).to.have.length(1);
-        expect(htmlButton.props().className).to.not.contain('sci-react-unstyled-button');
+        let wrapper = shallow(<Dialog isVisible={false} className="blue" isBaseStylesDisabled={true} />);
+        let rootElement = wrapper.first();
+        expect(rootElement.props().className).to.not.contain('sci-react-dialog');
     });
 
     it("Click event", (done) => {
         let props = {
-            onClick: done
+            onBackgroundClick: done
         };
 
-        let wrapper = shallow(<UnstyledButton {...props} />);
-        wrapper.simulate("click");
+        let wrapper = shallow(<Dialog isVisible={false} {...props} />);
+        wrapper.find(".overlay").simulate("click");
     });
 
     it("Class name override", () => {
-        let wrapper = shallow(<UnstyledButton className="blue" />);
-        let htmlButton = wrapper.find('button');
-        expect(htmlButton).to.have.length(1);
-        expect(htmlButton.props().className).to.contain('sci-react-unstyled-button');
-        expect(htmlButton.props().className).to.contain('blue');
+        let wrapper = shallow(<Dialog isVisible={false} className="blue" />);
+        let rootElement = wrapper.first();
+        expect(rootElement.props().className).to.contain('sci-react-dialog');
+        expect(rootElement.props().className).to.contain('blue');
     });
 });
--- src/component.tsx
+++ src/component.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 
 import * as classNames from "classnames";
+import { MouseEvent, KeyboardEvent } from "react";
 
-export interface IUnstyledButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    isBaseStylesDisabled?: boolean
+export interface IDialogProps extends React.HTMLAttributes<HTMLDivElement> {
+    onBackgroundClick?: () => void;
+    isBaseStylesDisabled?: boolean;
+    isVisible: boolean;
 }
 
-export class UnstyledButton extends React.Component<IUnstyledButtonProps> {
+export class Dialog extends React.Component<IDialogProps> {
     render() {
-        let { isBaseStylesDisabled, className, ...htmlProps } = this.props;
-        if (isBaseStylesDisabled !== true) {
-            className = classNames("sci-react-unstyled-button", className);
-        } else {
-            className = className;
-        }
+        let { isBaseStylesDisabled, onBackgroundClick, isVisible, className, ...htmlProps } = this.props;
+        className = classNames({ "sci-react-dialog": isBaseStylesDisabled !== true }, className, { "visible": this.props.isVisible });
 
-        return <button {...htmlProps} className={className} />;
+        return (
+            <div {...htmlProps} className={className}>
+                <div className="overlay" onClick={this.onBackgroundClick}>
+                    <div className="dialog" onClick={this.onDialogClick} onKeyDown={this.onKeyDown}>
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>);
+    }
+
+    private onKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+        console.log("Ricky rules");
+    }
+
+    private onDialogClick = (event: MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+    }
+
+    private onBackgroundClick = () => {
+        if (this.props.onBackgroundClick) {
+            this.props.onBackgroundClick();
+        }
     }
 }
